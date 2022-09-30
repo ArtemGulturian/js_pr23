0a47c8e36c0e40029665c55089c4644f81fe1a5d
art <gulturyan.a@gmail.com>
2022-09-30 13:05:54 +0300

    json learning

--- script.js
+++ script.js
@@ -2,9 +2,17 @@
 
 const persone = {
     name: 'Alex',
-    tel: '+3800000'
+    tel: '+3800000',
+    parents: {
+        mom: 'Olga',
+        dad: 'Mike'
+    }
 };
 
-console.log(JSON.stringify(persone));
+// console.log(JSON.stringify(persone));
 // console.log(JSON.parse(JSON.stringify(persone)));
+const clone = JSON.parse(JSON.stringify(persone));
 
+clone.parents.mom = 'Ann';
+console.log(persone);
+console.log(clone);
